b5a74dfcf1b24e3eee5af05c52c99466949c9a3e
teozoia <zoiateo@gmail.com>
2019-12-29 19:10:38 +0100

    Initial Commit

---
+++ chip9/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  chip9
+//
+//  Created by Matteo Zoia on 29/12/2019.
+//  Copyright © 2019 Matteo Zoia. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
